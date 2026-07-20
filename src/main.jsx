33a40f79437f8bbf0ad218efd6b2ceb49deb6429
Mohamed-Messaoudene <127661213+Mohamed-Messaoudene@users.noreply.github.com>
2026-07-20 19:20:38 +0100

    refact(ThemeContext): migrate from themeContex.jsx into .tsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { ThemeProvider } from "./context/themeContext.jsx";
+import { ThemeProvider } from "./context/themeContext";
 import { BrowserRouter } from "react-router-dom";
 
 const redirect = sessionStorage.redirect;
