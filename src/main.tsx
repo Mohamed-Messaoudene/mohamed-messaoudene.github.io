4b5e7fe9dbfe9b21b14ab747fccf752a70995aed
Mohamed-Messaoudene <127661213+Mohamed-Messaoudene@users.noreply.github.com>
2026-07-21 12:54:30 +0100

    fix(index.html): fix import stat,emt error of main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.js";
-import { ThemeProvider } from "./context/themeContext.js";
+import { ThemeProvider } from "./context/themeContext";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 const redirect = sessionStorage.redirect;
 
